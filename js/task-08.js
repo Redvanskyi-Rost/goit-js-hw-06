fe7c9d4fcc40b9d5c862fa8c93f0986d698719a7
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-11-10 16:05:12 +0200

    виправив виправлене

--- js/task-08.js
+++ js/task-08.js
@@ -18,9 +18,12 @@ function onSubmit(event) {
 
   if (email.value === "" || password.value === "") {
     alert("Please fill in all the fields!");
-  } else {
-    console.log(`{Email: ${email.value}, Password: ${password.value}}`);
-    event.currentTarget.reset();
   }
+  // Завдання 8 - при сабміті в консолі має бути обʼєкт з введеними в поля значеннями.
+  const userInput = {
+    Email: email.value,
+    Password: password.value,
+  };
+  console.log(userInput);
+  event.currentTarget.reset();
 }
-// Завдання 8 - при сабміті в консолі має бути обʼєкт з введеними в поля значеннями.
